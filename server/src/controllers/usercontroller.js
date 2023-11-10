e75dfafeed233ed7968793894b37ac8a8c58ad68
Manumanu1234 <manumanuvkm123@gmail.com>
2023-11-10 20:52:48 +0530

    cartapi

--- server/src/controllers/usercontroller.js
+++ server/src/controllers/usercontroller.js
@@ -1,5 +1,7 @@
 const { Router} = require('express');
-const { productModel, cartModel } = require('../database/schema');
+const { productModel, cartModel, userModel } = require('../database/schema');
+const { default: mongoose } = require('mongoose');
+var ObjectId = require("mongodb").ObjectId;
 
 
 let userrouter=Router()
@@ -17,7 +19,7 @@ userrouter.get('/product/all', async (req, res) => {
             var orgintotal=productarr[i].quntity;
             total=total+orgintotal;
         }
-        console.log(total);
+         console.log(total);
     }
 
         return res.json({data:product,cartcount:total});
@@ -40,19 +42,25 @@ userrouter.post("/addtocart",async(req,res)=>{
     let findcart=await cartModel.findOne({userid:req.body.userid})
 
     const product={
-        prodId:req.body.productid,
+        prodId:new ObjectId(req.body.productid),
         quntity:1
     }
+    
     if(findcart==null ||findcart.products==null || findcart.products==undefined){
       
         const cart=await new cartModel({userid:req.body.userid,products:[product]}).save();
+        console.log(cart)
     }else{
-        let sameelem=findcart.products.findIndex(product=>product.prodId==req.body.productid)
+        let convert=new ObjectId(req.body.productid)
+        console.log(convert)
+        console.log(findcart.products)
+        let sameelem=findcart.products.findIndex(product=>product.prodId.toString() === convert.toString())
         console.log(sameelem)
-        
+       
         if(sameelem !=-1){
-            await cartModel.updateOne({'products.prodId':req.body.productid},{
-                $inc: {'products.$.quntity':1}})
+
+            await cartModel.updateOne({'products.prodId':convert},{
+                $inc:{'products.$.quntity':1}})
         }else{
             await cartModel.updateOne({userid:req.body.userid},{
             $push:{"products":product}
@@ -61,6 +69,91 @@ userrouter.post("/addtocart",async(req,res)=>{
         }  
     }
 })
+
+
+  userrouter.get("/cart",async(req,res)=>{
+    let cartcoll=await cartModel.aggregate([
+        {
+            $match:{userid:"200"}
+        },
+        {
+            $unwind: "$products"
+        },
+        {
+            $project:{
+                item:"$products.prodId",
+                quntity:"$products.quntity"
+            }
+        },
+        {
+            $lookup:{
+                from:"products",
+                localField:"item",
+                foreignField:"_id",
+                as:"newcollection"
+            }
+
+        },
+        {
+            $project:{
+                item:1,
+                quntity:1,
+                productdetails: { $arrayElemAt: [ "$newcollection", 0 ] }
+            }
+        }
+
+    ]).exec()
+     console.log(cartcoll)
+  })
+
+
+userrouter.get("/usercart",(req,res)=>{
+   async function  totalprice(){
+        let totalproductprice=await cartModel.aggregate([
+        {
+            $match:{userid:"200"}
+        },
+        {
+            $unwind: "$products"
+        },
+        {
+            $project:{
+                item:"$products.prodId",
+                quntity:"$products.quntity"
+            }
+        },
+        {
+            $lookup:{
+                from:"products",
+                localField:"item",
+                foreignField:"_id",
+                as:"newcollection"
+            }
+
+        },
+        {
+            $project:{
+                item:1,
+                quntity:1,
+                productdetails: { $arrayElemAt: [ "$newcollection", 0 ] }
+            }
+        },
+        {
+            $group:{
+                _id:null,
+               total:{$sum:{$multiply:["$quntity","$productdetails.prodprice"]}}
+            }
+        }
+
+    ]).exec()
+     console.log(totalproductprice)
+    }
+    
+    
+})
+
+
+
 
 
 
