83ea539da0de6fb52c0cd48a9a906f57d825115b
SurajPa05 <surajpa72@gmail.com>
2023-11-10 18:23:50 +0530

    Cart Ui

--- Client/src/pages/test/hi.jsx
+++ Client/src/pages/test/hi.jsx
@@ -7,50 +7,97 @@ const ShoppingCart = () => {
                 {/* Nav bar */}
             </nav>
             <section>
-                <div className="ProductDetails">
+                <div class="ProductDetails">
+
                     <table>
-                        <thead>
+                        <tbody>
                             <tr>
                                 <th>Items</th>
                                 <th></th>
                                 <th>Quantity</th>
                                 <th>Remove</th>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/AVANT-P870DMG-GAMING-LAPTOP.png" alt="" /></td>
-                                <td>OMEN-P870DMG</td>
-                                <td>
-                                    <div className="pcount">
-                                        <div><button>+</button></div>
-                                        <div><span>5</span></div>
-                                        <div><button>-</button></div>
-                                    </div>
-                                </td>
-                                <td><button>i</button></td>
-                            </tr>
-                            {/* Repeat the above <tr> for other items */}
                         </tbody>
+
+                        <tr>
+                            <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/AVANT-P870DMG-GAMING-LAPTOP.png"
+                                alt="" /></td>
+                            <td>OMEN-P870DMG</td>
+                            <td>
+                                <div class="pcount">
+                                    <div><button>+</button></div>
+                                    <div><span>5</span></div>
+                                    <div><button>-</button></div>
+                                </div>
+                            </td>
+                            <td><button>i</button></td>
+                        </tr>
+
+                        <tr>
+                            <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/AVANT-P870DMG-GAMING-LAPTOP.png"
+                                alt="" /></td>
+                            <td>OMEN-P870DMG</td>
+                            <td>
+                                <div class="pcount">
+                                    <div><button>+</button></div>
+                                    <div><span>5</span></div>
+                                    <div><button>-</button></div>
+                                </div>
+                            </td>
+                            <td><button>i</button></td>
+                        </tr>
+
+
+                        <tr>
+                            <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/AVANT-P870DMG-GAMING-LAPTOP.png"
+                                alt="" /></td>
+                            <td>OMEN-P870DMG</td>
+                            <td>
+                                <div class="pcount">
+                                    <div><button>+</button></div>
+                                    <div><span>5</span></div>
+                                    <div><button>-</button></div>
+                                </div>
+                            </td>
+                            <td><button>i</button></td>
+                        </tr>
+
+
+                        <tr>
+                            <td><img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/AVANT-P870DMG-GAMING-LAPTOP.png"
+                                alt="" /></td>
+                            <td>OMEN-P870DMG</td>
+                            <td>
+                                <div class="pcount">
+                                    <div><button>+</button></div>
+                                    <div><span>5</span></div>
+                                    <div><button>-</button></div>
+                                </div>
+                            </td>
+                            <td><button>i</button></td>
+                        </tr>
+
                     </table>
                 </div>
-                <div className="CardAndCheckout">
+                <div class="CardAndCheckout">
                     <p>ORDER SUMMARY</p>
                     <p>ITEMS: <span>3</span></p>
                     <p>PROMO CODE:</p>
                     <input type="text" placeholder="AAA-BBB-YYY-CCC" />
-                    <div className="checkbtn">
-                        <button className="apply">APPLY</button>
+                    <div class="checkbtn">
+
+                        <button class="apply">APPLY</button>
                     </div>
-                    <div className="subtotal">
+
+                    <div class="subtotal">
                         <div>TOTAL COST :</div> <span>$ 1420</span>
                     </div>
-                    <div><button className="checkout">CHECKOUT</button></div>
+                    <div><button class="checkout">CHECKOUT</button></div>
+
                 </div>
             </section>
         </div>
-    );
-};
+    )
+}
 
 export default ShoppingCart;
-
