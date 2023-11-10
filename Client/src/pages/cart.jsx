8adac17e653a9b776db3fd1a110b0abc0628406e
Naveenravi07 <gamerkidnav@gmail.com>
2023-11-10 23:22:00 +0530

    Redirect to login page when accessing shop/cart

--- Client/src/pages/cart.jsx
+++ Client/src/pages/cart.jsx
@@ -1,11 +1,14 @@
-import axios from 'axios'
 import CartPage from '../components/cartpage.jsx'
 import instance from '@/Helpers/Config/axios.config.js'
 import { toast } from 'react-toastify'
 import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router.js'
 
 export default function Cart(){
+    let router = useRouter()
     let [cartData,setCartData] = useState(null)
+    let user = useSelector((state)=>state.auth)
 
     let fetch_user_cart = async() =>{
         await instance.get('/user/cart')
@@ -13,9 +16,14 @@ export default function Cart(){
         .catch((err)=>toast.error("Error occured"))
     }
     
-     useEffect(()=>{
-         fetch_user_cart()
-     },[])
+    useEffect(() => {
+        if(!user || user.token == null || user.loggedIn == false ){
+            toast.dark("You must login")
+            router.push('/auth/login')
+        }else{
+            fetch_user_cart()
+        }
+    }, [user])
 
     return(
         <CartPage cartData={cartData} />
