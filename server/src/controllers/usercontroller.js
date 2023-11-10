aa467a1614dfe918d29c28ef85d0c9b27b5863a3
SurajPa05 <surajpa72@gmail.com>
2023-11-10 19:03:04 +0530

    Bug Fixed

--- server/src/controllers/usercontroller.js
+++ server/src/controllers/usercontroller.js
@@ -1,64 +1,66 @@
-const { Router} = require('express');
+const { Router } = require('express');
 const { productModel, cartModel } = require('../database/schema');
 
 
-let userrouter=Router()
+let userrouter = Router()
 
 userrouter.get('/product/all', async (req, res) => {
+    console.log("/product/all called !");
     try {
         const product = await productModel.find()
-        const totalproductsincart=await cartModel.findOne({userid:"400"});
+        const totalproductsincart = await cartModel.findOne({ userid: "400" });
         console.log(totalproductsincart)
-    let total=0;
-   
-    if(totalproductsincart!=null){
-        let productarr=totalproductsincart.products;
-        for(let i=0;i<productarr.length;i++){
-            var orgintotal=productarr[i].quntity;
-            total=total+orgintotal;
-        }
-        console.log(total);
-    }
+        let total = 0;
 
-        return res.json({data:product,cartcount:total});
+        if (totalproductsincart != null) {
+            let productarr = totalproductsincart.products;
+            for (let i = 0; i < productarr.length; i++) {
+                var orgintotal = productarr[i].quntity;
+                total = total + orgintotal;
+            }
+            console.log(total);
+        }
+
+        return res.json({ data: product, cartcount: total });
     } catch (err) { console.log(err); }
 
-    
+
 });
 
 userrouter.get('/product/get', async (req, res) => {
     try {
         const product = await productModel.findById(req.query.id);
-        return res.json({data:product});
+        return res.json({ data: product });
     } catch (err) { console.log(err); }
-}); 
+});
 
 
 
 
-userrouter.post("/addtocart",async(req,res)=>{
-    let findcart=await cartModel.findOne({userid:req.body.userid})
+userrouter.post("/addtocart", async (req, res) => {
+    let findcart = await cartModel.findOne({ userid: req.body.userid })
 
-    const product={
-        prodId:req.body.productid,
-        quntity:1
+    const product = {
+        prodId: req.body.productid,
+        quntity: 1
     }
-    if(findcart==null ||findcart.products==null || findcart.products==undefined){
-      
-        const cart=await new cartModel({userid:req.body.userid,products:[product]}).save();
-    }else{
-        let sameelem=findcart.products.findIndex(product=>product.prodId==req.body.productid)
-        console.log(sameelem)
-        
-        if(sameelem !=-1){
-            await cartModel.updateOne({'products.prodId':req.body.productid},{
-                $inc: {'products.$.quntity':1}})
-        }else{
-            await cartModel.updateOne({userid:req.body.userid},{
-            $push:{"products":product}
+    if (findcart == null || findcart.products == null || findcart.products == undefined) {
 
-        })
-        }  
+        const cart = await new cartModel({ userid: req.body.userid, products: [product] }).save();
+    } else {
+        let sameelem = findcart.products.findIndex(product => product.prodId == req.body.productid)
+        console.log(sameelem)
+
+        if (sameelem != -1) {
+            await cartModel.updateOne({ 'products.prodId': req.body.productid }, {
+                $inc: { 'products.$.quntity': 1 }
+            })
+        } else {
+            await cartModel.updateOne({ userid: req.body.userid }, {
+                $push: { "products": product }
+
+            })
+        }
     }
 })
 
