dc1185fdd1f2daa65f0d005e3218dec3247d7d7f
Suraj <surajpa72@gmail.com>
2023-11-13 20:49:11 +0530

    Minor Changes V2

--- Client/src/pages/shop.jsx
+++ Client/src/pages/shop.jsx
@@ -35,7 +35,7 @@ export default function all() {
     return (
         <>
         <Navbarv2 />
-        <div style={{ display: 'flex', flexDirection: "row", flexWrap: "wrap", gap: '3rem', justifyContent: 'center', marginTop: '30rem' }}>
+        <div style={{ display: 'flex', flexDirection: "row", flexWrap: "wrap", gap: '3rem', justifyContent: 'center', marginTop: '75rem' }}>
             {
                 products && products.map((item) =>
                     <Card  prodPrice={item.prodprice} proId={item._id} prodName={item.prodName} prodDesc={item.proddesc} addtoCart={(id)=>add_to_cart(id)} />
