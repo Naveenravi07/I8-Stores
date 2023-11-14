a7f8ac577b21d3540bafc6ac17f3e48c8be9903e
Naveenravi07 <gamerkidnav@gmail.com>
2023-11-14 18:33:51 +0530

    Cors fix v2

--- Client/src/Helpers/Config/axios.config.js
+++ Client/src/Helpers/Config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://50.18.75.156:42069'
-})
+    baseURL: 'http://50.18.75.156:42069',
+},{Credential:'same-origin'})
 
 export default instance
